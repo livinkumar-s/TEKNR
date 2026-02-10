2c4e52f699ef21a39f18e983d35da5afeb7fc3fd
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-10 16:37:34 +0530

    JavaScript - introduction

---
+++ app.js
@@ -0,0 +1,18 @@
+// const age=22;
+// age=44
+
+// let age=22;
+// let age1=23;
+
+// console.log(age1);
+
+// let name1="123"
+// let val=4.4
+// let bool1=true
+// let a;
+// let b=null;
+
+let a=10
+a=11
+
+console.log(a);
