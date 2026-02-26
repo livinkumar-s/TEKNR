46d2053b07e1e54a6d3e35c811576d36c0041808
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-26 13:05:38 +0530

    Event Listeners, Traversal, Creating and Appending Child

--- app.js
+++ app.js
@@ -28,11 +28,69 @@
 // p.style.color = "red";
 // p.style.fontSize="50px";
 
-let paras=document.querySelectorAll(".para")
+// let paras=document.querySelectorAll(".para")
 // console.log(paras);
 // paras[1].classList.add("styledPara")
 // paras[1].classList.remove("styledPara")
 // paras[1].classList.toggle("styledPara")
-console.log(paras[1].classList.contains("styledPara"));
+// console.log(paras[1].classList.contains("styledPara"));
 
 
+// Creating Elements 
+
+// let newElement= document.createElement("div")
+// newElement.textContent="New Element"
+// newElement.classList.add("new")
+// console.log(newElement);
+
+// let body=document.querySelector("body")
+// body.appendChild(newElement)
+
+// let box=document.querySelector(".box")
+// let para=document.querySelector(".box>p")
+// box.remove()
+// box.removeChild(para)
+
+// DOM Traversal 
+
+// let ul=document.querySelector(".itemlist")
+// console.log(ul.parentElement);
+// console.log(ul.children);
+// console.log(ul.firstElementChild);
+// console.log(ul.lastElementChild);
+// let para=document.querySelector(".para")
+// console.log(para.previousElementSibling);
+// console.log(para.nextElementSibling);
+
+
+// console.log(document.querySelector("body").lastElementChild.previousElementSibling.lastElementChild.previousElementSibling);
+
+
+// function printHello(e){
+//     e.target.textContent="Dont Click me again...!"
+// }
+
+// let btn=document.querySelector("button")
+
+// btn.addEventListener("click",(e)=>{
+//     e.target.textContent="Dont Click me......!"
+// })
+
+// let form=document.querySelector("form")
+// form.addEventListener("submit",(e)=>{
+    //     e.preventDefault()
+// })
+
+// document.querySelector("#head").addEventListener("mouseover",()=>{
+    //     console.log("Hello");
+    // })
+    
+    // document.querySelector("#head").addEventListener("mouseleave",()=>{
+//     console.log("Hello");
+// })
+// let form=document.querySelector("form")
+
+// form.firstElementChild.addEventListener("input",(e)=>{
+//     console.log(e.target.value);
+    
+// })
