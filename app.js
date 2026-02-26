14aaec22655c5b69c3f0a8edeb76273f508d1480
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-26 13:29:33 +0530

    To Do App

--- app.js
+++ app.js
@@ -1,96 +1,26 @@
-// let h1=document.getElementById("head")
-// let p=document.getElementsByClassName("para")
-// let paras=document.getElementsByTagName("p")
-// console.log(paras);
+let addBtn=document.querySelector("#addTask")
+let inp=document.querySelector("input")
+let taskList=document.querySelector("ol")
 
-// let p1=document.querySelector(".para")
-// let p1=document.querySelectorAll(".para")
-// console.log(p1);
+addBtn.addEventListener("click",()=>{
+    let task=inp.value;
+    let newLi=document.createElement("li")
+    newLi.textContent=task
+    let deleteBtn=document.createElement("button")
+    deleteBtn.textContent="rem"
+    deleteBtn.addEventListener("click",(e)=>{
+        e.target.parentElement.remove()
+    })
+    newLi.appendChild(deleteBtn)
+    taskList.appendChild(newLi)
+    inp.value=""
+})
 
-// let h1=document.querySelector("#head")
-// h1.innerText="Hello world"
-// console.log(h1.textContent);
-// h1.textContent="Hi guys....!"
+// taking value from inp 
+// creae nerw li tag 
+// value as textContent of li tag
+// creating new btn tag and adding textcontent to it 
+// add li as child of ol tag
+//removing text from inp tag
 
-
-// let box=document.querySelector(".box")
-
-// console.log(box.textContent);
-// console.log(box.innerHTML);
-// box.innerHTML="HI <h2>Dummy text</h2>"
-
-// let h1=document.getElementById("head")
-// console.log(h1.getAttribute("id"));
-// h1.setAttribute("id","head2")
-// h1.removeAttribute("id")
-
-// let p = document.querySelector(".para");
-// p.style.color = "red";
-// p.style.fontSize="50px";
-
-// let paras=document.querySelectorAll(".para")
-// console.log(paras);
-// paras[1].classList.add("styledPara")
-// paras[1].classList.remove("styledPara")
-// paras[1].classList.toggle("styledPara")
-// console.log(paras[1].classList.contains("styledPara"));
-
-
-// Creating Elements 
-
-// let newElement= document.createElement("div")
-// newElement.textContent="New Element"
-// newElement.classList.add("new")
-// console.log(newElement);
-
-// let body=document.querySelector("body")
-// body.appendChild(newElement)
-
-// let box=document.querySelector(".box")
-// let para=document.querySelector(".box>p")
-// box.remove()
-// box.removeChild(para)
-
-// DOM Traversal 
-
-// let ul=document.querySelector(".itemlist")
-// console.log(ul.parentElement);
-// console.log(ul.children);
-// console.log(ul.firstElementChild);
-// console.log(ul.lastElementChild);
-// let para=document.querySelector(".para")
-// console.log(para.previousElementSibling);
-// console.log(para.nextElementSibling);
-
-
-// console.log(document.querySelector("body").lastElementChild.previousElementSibling.lastElementChild.previousElementSibling);
-
-
-// function printHello(e){
-//     e.target.textContent="Dont Click me again...!"
-// }
-
-// let btn=document.querySelector("button")
-
-// btn.addEventListener("click",(e)=>{
-//     e.target.textContent="Dont Click me......!"
-// })
-
-// let form=document.querySelector("form")
-// form.addEventListener("submit",(e)=>{
-    //     e.preventDefault()
-// })
-
-// document.querySelector("#head").addEventListener("mouseover",()=>{
-    //     console.log("Hello");
-    // })
-    
-    // document.querySelector("#head").addEventListener("mouseleave",()=>{
-//     console.log("Hello");
-// })
-// let form=document.querySelector("form")
-
-// form.firstElementChild.addEventListener("input",(e)=>{
-//     console.log(e.target.value);
-    
-// })
+//<li>task <button>Rem</button></li>
